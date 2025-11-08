878c40e4faf4a1117cd40d9a742ee8c089b0f5de
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-08 19:59:53 +0530

    Create 08_day2.py

---
+++ 2025/11-November/08_day2.py
@@ -0,0 +1,14 @@
+#Taking user input and type conversion
+
+a = input("Enter our Name : ")
+print("Welcome ",a)
+
+name = input("Enter your Name : ")
+age = int(input("Enter your Age :"))
+marks = float(input("Enter your Marks : "))
+print("Your name is :",name)
+print("Your agae is : ",age)
+print("Your Marks are : ",marks)
+print(type(name,))
+print(type(age))
+print(type(marks))
