d142b5638daac2b9521ffa1df8eed609fe56c691
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-09 15:07:47 +0530

    Create 2025-11-09_day3.py

---
+++ 2025/11-November/2025-11-09_day3.py
@@ -0,0 +1,60 @@
+    #    Arithematic Operators 
+
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+    #  Relational Operators
+
+a = 50
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+        # Assignment Operator
+
+num = 10
+num = num + 10
+print(num)
+
+    #  or 
+
+number = 10
+number += 10
+print(number)
+number -= 10
+print(number)
+number *= 10
+print(number)
+number /= 10
+print(number)
+number %= 10
+print(number)
+ # logic 
+number = 10
+number **= 5
+print(number)
+
+        #  Logical Operators
+
+print(not False)
+print(not True)
+
+la = 50
+lb = 30
+print(not (la > lb))
+
+val1 = True
+val2 = False
+print("AND operator: ",val1 and val2)
+print("OR operator: ",(la==lb) or (la>lb))
+
