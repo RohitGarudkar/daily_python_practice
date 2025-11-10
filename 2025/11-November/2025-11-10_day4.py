d5229af2b044f11591efd92fc0d701006cc2c0ce
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-10 15:49:29 +0530

    Create 2025-11-10_day4.py

---
+++ 2025/11-November/2025-11-10_day4.py
@@ -0,0 +1,38 @@
+str = "hello \t\n world "
+print(str)
+
+str1 = "Rohit"
+str2 = "Garudkar"
+print(str1+" "+str2)
+
+print(len(str1))
+
+    # slicing 
+
+str = "Rohit Garudkar"
+print(str[0:5])
+print(str[-8:-1])
+
+    # string functions
+
+str = "rock Garudkar"
+print(str.endswith("ar"))
+print(str.capitalize())
+print(str.replace("rock","Rohit"))
+print(str.find("o"))
+print(str.count("k"))
+
+
+#type conversion 
+a = "2"
+b = 4.25
+
+a = int(a)
+sum = a + b
+print(sum)
+
+c = int(a)
+c = float(a)
+c = str(a)
+
+
