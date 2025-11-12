eff71f63d9421cf629d51a5c02f28013cfbdc05d
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-12 16:19:45 +0530

    Create 2025-11-12_day6.py

---
+++ 2025/11-November/2025-11-12_day6.py
@@ -0,0 +1,17 @@
+collectoin = {1,2,3,4}
+collectoin.add(5)
+print(collectoin)
+collectoin.remove(5)
+print(collectoin)
+collectoin.clear()
+print(collectoin)
+collectoin = {1,2,3,4}
+print(collectoin)
+collectoin.pop()
+print(collectoin)
+
+
+colle_1 = {1,2,3,4}
+colle_2 = {"Rohit","Garudkar",1,2,3}
+print(colle_1.intersection(colle_2)) 
+print(colle_1.union(colle_2)) 
