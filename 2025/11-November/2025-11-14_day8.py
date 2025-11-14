ddb142aac973c6b9337906be60c0a576dc0cc4fd
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-14 20:30:58 +0530

    Update 2025-11-14_day8.py

--- 2025/11-November/2025-11-14_day8.py
+++ 2025/11-November/2025-11-14_day8.py
@@ -11,3 +11,25 @@ if( num == 4):
     print("number is 4")
 if( num == 5):
     print("number is 5")
+
+    #if - else
+
+age = int(input("Enter your age :"))
+if(age >= 18):
+    print("can drive")
+    print("can vote ")
+else:
+    print("can not drive ")
+    print("can not vote")
+
+    #if - elif - else
+
+color = input("Enter color : ")
+if( color == "red"):
+    print("stop")
+elif(color == "yellow"):
+    print("look for ")
+elif(color == "green" ):
+    print("go")
+else:
+    print("light is broken")
