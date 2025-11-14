11f1f702f34631ffc699eb5df0dade6e13acd429
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-14 20:27:34 +0530

    Create 2025-11-14_day8.py

---
+++ 2025/11-November/2025-11-14_day8.py
@@ -0,0 +1,13 @@
+    #if 
+     
+num = int(input("Enter number between 1 to 5 :"))
+if( num == 1):
+    print("number is 1")
+if( num == 2):
+    print("number is 2")
+if( num == 3):
+    print("number is 3")
+if( num == 4):
+    print("number is 4")
+if( num == 5):
+    print("number is 5")
