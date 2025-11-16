5f90a595731d624adf01bcbdb30e4112b3aaece0
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-16 11:24:12 +0530

    Create 2025-11-16_day10.py

---
+++ 2025/11-November/2025-11-16_day10.py
@@ -0,0 +1,22 @@
+#breake and continue 
+
+i = 1
+while i <= 5:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+print("------------------------")
+
+j = 1
+while j <= 10:
+   
+    if (j%2 == 1):
+        j += 1
+        continue
+    print(j)
+    j += 1
+   
+        
+    
