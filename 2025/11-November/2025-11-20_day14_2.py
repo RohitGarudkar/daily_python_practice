58416e21e7c4cd94404da24f3edeea99e186ab31
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-20 19:18:31 +0530

    Create 2025-11-20_day14_2.py

---
+++ 2025/11-November/2025-11-20_day14_2.py
@@ -0,0 +1,34 @@
+#diamond pattern 
+
+n =  5
+
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end= " ")
+    
+    for j in range(i+1):
+        print("*",end=" ")
+
+    for j in range(i):
+        print("*",end=" ")
+    
+    for j in range(n,i,-1):
+        print(" ",end=" ")
+
+    print()
+
+for i in range(n,-1,-1):
+    for j in range(i,n):
+        print(" ",end= " ")
+    
+    for j in range(i+1):
+        print("*",end=" ")
+
+    for j in range(i):
+        print("*",end=" ")
+    
+    for j in range(n,i,-1):
+        print(" ",end=" ")
+
+    print()
+
