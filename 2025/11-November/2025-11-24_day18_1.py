5c0e6eca446a7cd53dc671c61827a64c693a3f2f
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-24 20:30:23 +0530

    Create 2025-11-24_day18_1.py

---
+++ 2025/11-November/2025-11-24_day18_1.py
@@ -0,0 +1,16 @@
+#Exception Handeling 
+def my_gen():
+    print("First Item")
+    yield 10
+    print("Second Item")
+    yield 20
+    print("Third Item")
+    yield 30
+    print("Fourth Item")
+    yield 40
+gen = my_gen()
+
+while True:
+    try:
+        print(next(gen))
+    except StopIteration: break
