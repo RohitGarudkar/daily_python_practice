a3b26906f5f90887cf5e39200cac29f5271a7c20
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-29 11:58:32 +0530

    Update 2025-11-28_day21_1.py

--- 2025/11-November/2025-11-28_day21_1.py
+++ 2025/11-November/2025-11-28_day21_1.py
@@ -1,1 +1,12 @@
-wait
+#Program to Identify Odd and Even Numbers Using a Generator Function
+def odd_even(l):
+    for i in l:
+        if i%2 == 0:
+            yield f"{i} is even "
+        else: 
+            yield f"{i} is odd "
+
+num = [int(i) for i in input("Enter the numver ").split(",")]
+even_odd_object = odd_even(num)
+for i in even_odd_object:
+    print(i)
