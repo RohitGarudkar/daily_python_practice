ffe956959293bcee446539b958ac887102d1cb14
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-29 11:59:38 +0530

    Update 2025-11-29_day22_1.py

--- 2025/11-November/2025-11-29_day22_1.py
+++ 2025/11-November/2025-11-29_day22_1.py
@@ -1,1 +1,14 @@
-wait
+#Program Demonstrating Function Decorator to Add Extra Behavior Before and After a Function
+def my_decorator(fn):
+    def inner():
+        print("Good Morning...")
+        fn()
+        print("How are you ?")
+    return inner
+
+@my_decorator
+def greet():
+    name = input("Enter your Name : ")
+    print(f"hello {name}")
+
+greet()
