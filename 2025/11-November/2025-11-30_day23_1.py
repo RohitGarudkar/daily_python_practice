b61d82ec9419c3d90136a6676ab8a9d518010091
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-30 10:35:28 +0530

    Update 2025-11-30_day23_1.py

--- 2025/11-November/2025-11-30_day23_1.py
+++ 2025/11-November/2025-11-30_day23_1.py
@@ -1,1 +1,8 @@
-wait
+#Program to Calculate the Area and Circumference of a Circle
+def circle(r):
+    pi = 3.14  
+    print("Area of a circle :",pi*r*r)
+    print("Cricumerference of a circle :",round((2*pi*r),2))
+    
+num = int(input("Enter the redius of the circle to print the are and curcumference "))
+circle(num)
