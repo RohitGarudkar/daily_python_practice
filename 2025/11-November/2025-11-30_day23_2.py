d6c04fbfaf87a9e2cf7d606a05aae6d42234324a
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-11-30 10:39:54 +0530

    Update 2025-11-30_day23_2.py

--- 2025/11-November/2025-11-30_day23_2.py
+++ 2025/11-November/2025-11-30_day23_2.py
@@ -1,1 +1,14 @@
-wait
+#import math 
+import math 
+math.radians(30)
+math.degrees(math.pi/6)
+math.sin(0.5235987755982988)
+math.cos(0.5235987755982988)
+math.tan(0.5235987755982988)
+math.log(10)
+math.log10(10)
+math.e**10
+math.pow(2,8)
+math.sqrt(27)
+math.ceil(1.4)
+math.floor(1.4)
