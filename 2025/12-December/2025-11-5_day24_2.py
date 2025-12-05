a2d71d214c2be258ffa12a8697ad93c4c0e759fe
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-05 23:30:47 +0530

    Create 2025-11-5_day24_2.py

---
+++ 2025/12-December/2025-11-5_day24_2.py
@@ -0,0 +1,14 @@
+#Random Modules
+import random
+random.random()
+random.randint(1,100)
+random.randrange(1,10)
+random.randrange(1,10,2)
+random.randrange(1,10,3)
+random.choice('computer')
+random.choice([1,2,3,4,5,6,7,8,9,10])
+random.choice(range(1,11))
+
+n = [1,2,3,4,5,6,7,8,9,10]
+random.shuffle(n)
+print(n)
