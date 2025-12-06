404f6aac1a6f60e6ad90cfc80e54eb1305fbdda1
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-06 18:28:13 +0530

    Create 2025-11-6_day25_1.py

---
+++ 2025/12-December/2025-11-6_day25_1.py
@@ -0,0 +1,11 @@
+#PYTHON OS MODULE 
+import os 
+os.getcwd()
+os.mkdir("C:/msg")
+os.chdir("C:/msg")
+os.getcwd()
+os.chdir("C:/Users/rohit/python")
+os.getcwd()
+os.rmdir("C:/msg")
+os.listdir("C:/Users/rohit/python")
+os.listdir("C:/Users/rohit")
