ca1e79063dfd6685690b9c329217bbc7aa41cbeb
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-07 23:09:04 +0530

    Create 2025-11-7_day26_2.py

---
+++ 2025/12-December/2025-11-7_day26_2.py
@@ -0,0 +1,7 @@
+#PYTHON STATISTICS MODULE
+import statistics
+statistics.mean([1,2,3,4,5,6,7])
+statistics.median([1,2,3,4,5,6,7])
+statistics.median([1,2,3,4,5,6,7,8])
+statistics.mode([2,5,3,2,8,3,9,4,2,5,6,5,1,5,2,5])
+statistics.stdev([1,1,5,2,2,5,3,3,5,4,4,5,5])
