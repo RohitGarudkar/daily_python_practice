1e2201b281a18a7baad8ec17f3f34b99ae17efd9
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-08 19:55:17 +0530

    Update 2025-11-8_day27_2.py

--- 2025/12-December/2025-11-8_day27_2.py
+++ 2025/12-December/2025-11-8_day27_2.py
@@ -5,3 +5,18 @@
 #except ....
 #    {run this code if exception occures}
 
+try:
+    a = int(input("Enter number A : "))
+    b = int(input("Enter number B : "))
+    c = a/b
+    print(f"{a}/{b} = {c}")
+
+#WITH EXCEPTION VARIABLE (e)
+except Exception as e :
+    print("Division by zero not allowed..")
+    print(e)
+
+#EXECUTED IF TRY BLOCK RUNS OTHREWISE NOT 
+else:
+    print("i am in other part of the program")
+    print("I am in Else part... When there is no error in try block")
