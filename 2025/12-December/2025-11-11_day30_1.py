cf50b8844ef34f13e1ea7db94465d1913d65e9e4
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-11 20:53:32 +0530

    Create 2025-11-11_day30_1.py

---
+++ 2025/12-December/2025-11-11_day30_1.py
@@ -0,0 +1,19 @@
+#Declaring Multiple Exceptions
+try:
+    a = 10
+    b = 'p'
+    c = a/b
+except (ArithmeticError) as e:
+    print(e)
+except (IOError) as e:
+    print(e)
+except (TypeError) as e:
+    print(e)
+except (NameError) as e:
+    print(e)
+except (ZeroDivisionError) as e:
+    print(e)
+except (IntendationError) as e:
+    print(e)
+else:
+    print("I am in Else part")
