900edf78d45c648b1ec2c7930068322b39076d32
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-14 12:31:03 +0530

    Update 2025-11-14_day33_1.py

--- 2025/12-December/2025-11-14_day33_1.py
+++ 2025/12-December/2025-11-14_day33_1.py
@@ -1,1 +1,15 @@
-w
+
+#Exception Handling try...finally block
+
+try:
+    fp = open("test.txt","r")
+    try:
+        fp.write("HI I am good")
+    finally:
+        fp.close()
+        print("file closed...")
+except Exception as e :
+    print("file mode error ")
+    print(e)
+
+print("I am in other part")
