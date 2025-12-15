14d6612339c816e3caa2147c523e4655a98f4699
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-15 20:24:26 +0530

    Create 2025-11-15_day35_2.py

---
+++ 2025/12-December/2025-11-15_day35_2.py
@@ -0,0 +1,21 @@
+#Custome Exception
+
+
+class MyError(Exception):
+    
+    #constroctor
+    def __init__(self,value):
+        self.value = value
+
+    #__str__ is to print the value
+    def __str__(self):
+        return(repr(self.value))
+try:
+    x = int(input("Enter a number upto 100 "))
+    if x>100:
+        raise MyError(x)
+    else:
+        print(f"{x} is valid number ")
+except MyError as e:
+    print(f"Exception occured :Invalid Number : {e}")
+    
