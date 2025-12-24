e784be2c9cfd7dcf89932f427869d01859659b4d
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-24 10:11:03 +0530

    Update 2025-11-23_day36_1.py

--- 2025/12-December/2025-11-23_day36_1.py
+++ 2025/12-December/2025-11-23_day36_1.py
@@ -1,1 +1,17 @@
-w
+class InvalidAge(Exception):
+    
+    #constroctor
+    def __init__(self,age):
+        self.age = age
+
+    #__str__ is to print the value
+    def __str__(self):
+        return(repr(self.age))
+try:
+    age = int(input("Ente age :"))
+    if age < 18:
+        raise InvalidAge(age)
+    else:
+        print(f"{age} is valid")
+except InvalidAge as e:
+    print(f"Exception occured : Invalid AGE : {age}")
