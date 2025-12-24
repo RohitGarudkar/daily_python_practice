58f73be61601f31386b7c1b1c93b46e9c1ba5b67
Rohit Garudkar <rohitgarudakar@gmail.com>
2025-12-24 10:20:20 +0530

    Create 2025-11-24_day37_1.py

---
+++ 2025/12-December/2025-11-24_day37_1.py
@@ -0,0 +1,20 @@
+#Handling Custom Errors Using Exception Class
+
+class ErrorInCode(Exception):
+    
+    #constroctor
+    def __init__(self,data):
+        self.data = data
+
+    #__str__ is to print the value
+    def __str__(self):
+        return(repr(self.data))
+try:
+    a = int(input("Enter value for A : "))
+    b = int(input("Enter value for B : ")) 
+    if a == b:
+        raise ErrorInCode(505)
+    else:
+        print("No error in code")
+except ErrorInCode as e:
+    print(f"Exception occured : Received Error : {e}")
