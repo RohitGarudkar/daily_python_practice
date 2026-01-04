3e92c9b914c39fe12ce1b09923ab37818c84f150
Rohit Garudkar <rohitgarudakar@gmail.com>
2026-01-04 13:28:35 +0530

    Update 2025-01-03_day41_1.py

--- 2026/01-January/2025-01-03_day41_1.py
+++ 2026/01-January/2025-01-03_day41_1.py
@@ -1,1 +1,21 @@
-w
+login_count = {}
+
+for log in logs:
+    parts = log.split()
+    user = parts[1]
+
+    login_count[user] = login_count.get(user, 0) + 1
+
+most_user = None
+most_count = 0
+
+for user, count in login_count.items():
+    if count > most_count:
+        most_user = user
+        most_count = count
+
+print("Login counts:")
+for user, count in login_count.items():
+    print(user, "->", count)
+
+print("\nMost active user:", most_user, "with", most_count, "logins")
