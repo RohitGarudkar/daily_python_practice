a405a4a4274f379b71fec4113ffa5caa55e13cf3
Rohit Garudkar <rohitgarudakar@gmail.com>
2026-01-04 13:27:04 +0530

    Update 2025-11-30_day40_1.py

--- 2025/12-December/2025-11-30_day40_1.py
+++ 2025/12-December/2025-11-30_day40_1.py
@@ -1,1 +1,13 @@
-w
+from random import *
+
+secret = randint(1, 10)
+
+while True:
+    guess = int(input("Guess a number (1–10): "))
+    if guess == secret:
+        print("Correct!")
+        break
+    elif guess < secret:
+        print("Too low")
+    else:
+        print("Too high")
