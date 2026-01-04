2fcf5948ac19fda5124407eab3085e78c0f30f09
Rohit Garudkar <rohitgarudakar@gmail.com>
2026-01-04 13:29:32 +0530

    Update 2025-01-04_day42_1.py

--- 2026/01-January/2025-01-04_day42_1.py
+++ 2026/01-January/2025-01-04_day42_1.py
@@ -1,1 +1,32 @@
-w
+#Problem: Task Scheduler (Greedy + Data Structures)
+
+from heapq import *
+
+tasks = [
+    ("A", 4, 3),
+    ("B", 3, 1),
+    ("C", 5, 2),
+    ("D", 6, 1),
+    ("E", 7, 3),
+]
+
+# sort by deadline
+tasks.sort(key=lambda x: x[1])
+
+current_time = 0
+max_heap = []  # store (-duration, task_name)
+
+for name, deadline, duration in tasks:
+    current_time += duration
+    heappush(max_heap, (-duration, name))
+
+    if current_time > deadline:
+        longest = heappop(max_heap)
+        current_time += longest[0]  # subtract duration
+
+scheduled_tasks = [name for _, name in max_heap]
+
+print("Tasks completed:", len(scheduled_tasks))
+print("Scheduled tasks:", scheduled_tasks)
+
+
